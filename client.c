154c26398286cc26e8e8dbcef12c47fabe4bc447
Travis Russell <travis.russell@maine.edu>
2023-03-26 20:13:58 -0400

    adding semaphores

--- client.c
+++ client.c
@@ -3,8 +3,17 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <string.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
+
+#define SEMNAMEONE "/sem-1001-start"
+#define SEMNAMETWO "/sem-1001-done"
 
 int main (int argc, char *argv[]){
+
+	sem_t* semStart = sem_open(SEMNAMEONE, 0);
+	sem_t* semDone = sem_open(SEMNAMETWO, 0);
 
 	float val[10] = { 1.0 };
 	
@@ -26,11 +35,15 @@ int main (int argc, char *argv[]){
 		return -1;
 	}
 
-	// copy some values to the block
-	memcpy(val, data, 10*sizeof(float));
-
-	for (int i = 0; i < 10; i++)
-		printf("%f\n", val[i]);
+	while(1){
+		sem_wait(semStart);
+		// copy some values to the block
+		memcpy(val, data, 10*sizeof(float));
+		for (int j = 0; j < 10; j++)
+			printf("%f\n", val[j]);
+		printf("\n");
+		sem_post(semDone);
+	}
 
 	// detach
 	shmdt(data);
--- server.c
+++ server.c
@@ -6,11 +6,20 @@
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <semaphore.h>
+#include <unistd.h>
+
+#define SEMNAMEONE "/sem-1001-start"
+#define SEMNAMETWO "/sem-1001-done"
 
 int main (int argc, char *argv[]){
 
-	float val[10] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
-//	float val[10] = { 0.0 };
+	sem_t *semStart = sem_open(SEMNAMEONE, O_CREAT, 0644, 0);
+	sem_t *semDone = sem_open(SEMNAMETWO, O_CREAT, 0644, 1);
+
+	float val[4][10] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,
+			 -1.0,0.0,-1.0,0.0,-1.0,0.0,-1.0,0.0,-1.0,0.0,
+			 1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,	
+			 -1.0,0.0,-1.0,0.0,-1.0,0.0,-1.0,0.0,-1.0,0.0};
 	
 	// get the key to the file
 	key_t key = ftok("shared_1000.txt", 0);
@@ -33,11 +42,22 @@ int main (int argc, char *argv[]){
 		return -1;
 	}
 
-	// copy some values to the block
-	memcpy(data, val, 10*sizeof(float));
+	int i = 0;
+	int j = 0;
+	while(i<20){
+		j = (i % 4);	
+		sem_wait(semDone);
+		memcpy(data, val[j], 10*sizeof(float));
+		sleep(1);
+		sem_post(semStart);
+		i++;
+	}
 
 	// detach
 	shmdt(data);
+
+	sem_close(semStart);
+	sem_close(semDone);
 
 	return 0;
 }
